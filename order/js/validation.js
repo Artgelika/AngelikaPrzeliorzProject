f78c8362140b35203a6087848b31d778b01758c8
Artgelika <artgelika.com>
2022-07-03 23:28:05 +0200

    Add regex into forms validation

--- order/js/validation.js
+++ order/js/validation.js
@@ -1,162 +1,64 @@
-	//Validtion Code For Inputs
+var username = document.forms['form']['username'];
+var email = document.forms['form']['email'];
+var message = document.forms['form']['message'];
 
-    var email = document.forms['form']['email'];
-    var password = document.forms['form']['password'];
-    var username = document.forms['form']['username'];
-    
-    var email_error = document.getElementById('email_error');
-    var username_error = document.getElementById('username_error')
-    var pass_error = document.getElementById('pass_error');
-    
-    email.addEventListener('textInput', email_Verify);
-    username.addEventListener('textInput', username_Verify);
-    password.addEventListener('textInput', pass_Verify);
-    
-    function validated(){
-        if (username.value.length < 3) {
-            username.style.border = "1px solid red";
-            username_error.style.display = "block";
-            username.focus();
-            return false;
-        }
-        if (email.value.length < 9) {
-            email.style.border = "1px solid red";
-            email_error.style.display = "block";
-            email.focus();
-            return false;
-        }
-        if (password.value.length < 6) {
-            password.style.border = "1px solid red";
-            pass_error.style.display = "block";
-            password.focus();
-            return false;
-        }
-    
+var email_error = document.getElementById('email_error');
+var username_error = document.getElementById('username_error')
+var message_error = document.getElementById('message_error');
+
+email.addEventListener('textInput', email_Verify);
+username.addEventListener('textInput', username_Verify);
+password.addEventListener('textInput', message_Verify);
+
+
+function validated() {
+    if (username.value.length < 3 || !valid_Name(username.value)) {
+        username.style.border = "1px solid red";
+        username_error.style.display = "block";
+        username.focus();
     }
-    function username_Verify(){
-        if (username.value.length >= 3) {
-            username.style.border = "1px solid silver";
-            username_error.style.display = "none";
-            return true;
-        }
+    if (email.value.length < 9 || !valid_Email(email.value)) {
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
     }
-
-    function email_Verify(){
-        if (email.value.length >= 8) {
-            email.style.border = "1px solid silver";
-            email_error.style.display = "none";
-            return true;
-        }
+    if (message.value.length < 6) {
+        message.style.border = "1px solid red";
+        message_error.style.display = "block";
+        message.focus();
     }
-    function pass_Verify(){
-        if (password.value.length >= 5) {
-            password.style.border = "1px solid silver";
-            pass_error.style.display = "none";
-            return true;
-        }
+}
+
+function username_Verify() {
+    if (username.value.length >= 3 && username.value.length <=20) {
+        username.style.border = "1px solid silver";
+        username_error.style.display = "none";
+        return true;
     }
-    
-    
+}
 
+function email_Verify() {
+    if (email.value.length >= 8 ) {
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
 
-// const form = document.getElementById('contact');
-// var username = document.forms['contact']['username'].value;
-// var email = document.forms['contact']['email'].value;
-// var message = document.forms['contact']['message'].value;
+function message_Verify() {
+    if (message.value.length >= 5) {
+        message.style.border = "1px solid silver";
+        message_error.style.display = "none";
+        return true;
+    }
+}
 
-// var name_error = document.getElementById('name_error');
-// var email_error = document.getElementById('email_error');
-// var message_error = document.getElementById('message_error');
+function valid_Name(username){
+    const regexUsernamePattern = new RegExp('(^[A-Z][a-z]{,19})$');
+    return username.match(regexUsernamePattern);
+}
 
-// username.addEventListener('textInput', username_Verify);
-// email.addEventListener('textInput', email_Verify);
-// message.addEventListener('textInput', message_Verify);
-
-
-
-// function validated(){
-    
-//     if ( username.length < 20)
-//     {
-//         alert("Name must be filled out");
-//         return false;
-//     }
-// 	// if (username.value.length < 9) {
-// 	// 	username.order.border = "1px solid red";
-// 	// 	username_error.style.display = "block";
-// 	// 	username.focus();
-// 	// 	return false;
-// 	// }
-
-//     // if (email.value.length < 9) {
-// 	// 	email.order.border = "1px solid red";
-// 	// 	email_error.style.display = "block";
-// 	// 	email.focus();
-// 	// 	return false;
-// 	// }
-
-//     // if (message.value.length < 9) {
-// 	// 	message.order.border = "1px solid red";
-// 	// 	message_error.style.display = "block";
-// 	// 	message.focus();
-// 	// 	return false;
-// 	// }
-// }
-
-// function email_Verify(){
-// 	if (email.length >= 5) {
-
-// 		return true;
-// 	}
-// }
-
-
-// // const form = document.getElementById("form");
-// // const username = document.getElementById("name");
-// // const email = document.getElementById("email");
-// // const message = document.getElementById("message");
-
-// // form.addEventListener("submit", (e) => {
-// //     let messages = []
-// //     e.preventDefault();
-// //     checkInputs();
-// // })
-
-// // function checkInputs() {
-// //     const formValue = form.value.trim();
-// //     const usernameValue = username.value.trim();
-// //     const emailValue = email.value.trim();
-// //     const messageValue = message.value.trim();
-
-// //     if (usernameValue === '' || usernameValue == null) {
-// //         messages.push("Name is required")
-// //     } else {
-// //         setSuccessFor(username);
-
-// //     }
-
-// //     if (emailValue === '') {
-// //         setErrorFor(emailValue, "Username can't be blank");
-// //     } else {
-// //         setSuccessFor(emailValue);
-// //     }
-
-// // }
-
-// // function setErrorFor(input, message) {
-// //     const formControl = input.parentElement;
-// //     const small = formControl.querySelector('small');
-
-// //     small.innerText = message;
-
-// //     formControl.className = 'form-control error';
-// // }
-
-// // function setSuccessFor(input) {
-// //     const formControl = input.parentElement;
-// //     formControl.className = 'form-control success';
-// // }
-
-// // function isEmail(email) {
-// //     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-// // }
+function valid_Email(email){
+    const regexEmailPattern = new RegExp('([A-Z])\w+.+@+([a-z])+.+([a-z]{2,3})');
+    return email.match(regexEmailPattern);
+}
